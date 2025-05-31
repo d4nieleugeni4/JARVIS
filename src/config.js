d1dbc0c212892ac1fb441961524fb1b36029dec2
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-05-31 19:29:13 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,42 +1,42 @@
-const path = require("path");
+//const path = require("path");
 
 // Prefixo dos comandos.
-exports.PREFIX = ".";
+//exports.PREFIX = ".";
 
 // Emoji do bot (mude se preferir).
-exports.BOT_EMOJI = "🤖";
+//exports.BOT_EMOJI = "🤖";
 
 // Nome do bot (mude se preferir).
-exports.BOT_NAME = "Jarvis";
+//exports.BOT_NAME = "Jarvis";
 
 // Número do bot. Coloque o número do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.BOT_NUMBER = "5524981321901";
+//exports.BOT_NUMBER = "5524981321901";
 
 // Número do dono do bot. Coloque o número do dono do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.OWNER_NUMBER = "5524981321901";
+//exports.OWNER_NUMBER = "5524981321901";
 
 // Diretório dos comandos
-exports.COMMANDS_DIR = path.join(__dirname, "commands");
+//exports.COMMANDS_DIR = path.join(__dirname, "commands");
 
 // Diretório de arquivos de mídia.
-exports.ASSETS_DIR = path.resolve(__dirname, "..", "assets");
+//exports.ASSETS_DIR = path.resolve(__dirname, "..", "assets");
 
 // Diretório de arquivos temporários.
-exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
+//exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
 
 // Timeout em milissegundos por evento (evita banimento).
-exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
+//exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 
 // Plataforma de API's
-exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
+//exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
 
 // Obtenha seu token, criando uma conta em: https://api.spiderx.com.br.
-exports.SPIDER_API_TOKEN = "9f25cwORge8CApnzRIlA";
+//exports.SPIDER_API_TOKEN = "9f25cwORge8CApnzRIlA";
 
 // Caso queira responder apenas um grupo específico, coloque o ID dele aqui (ex: 120363023799506419@g.us). Apenas para testes internos!
-exports.ONLY_GROUP_ID = "";
+//exports.ONLY_GROUP_ID = "";
 
 // Diretório base do projeto.
-exports.BASE_DIR = path.resolve(__dirname);
+//exports.BASE_DIR = path.resolve(__dirname);
